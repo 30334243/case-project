04bab5168f133f23e5e7d919d9802d03af5ebe3d
bvs <baboshinvs@spo-department.ru>
2025-01-24 10:10:26 +0300

    Добавил define import export

--- library/include/example.h
+++ library/include/example.h
@@ -1,4 +1,19 @@
 #include <cstdint>
 #include <fmt/core.h>
 
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
+#else
+    //  do nothing and hope for the best?
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
 void dummy(int val);
