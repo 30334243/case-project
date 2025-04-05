9c194030f07f2ac370b21ca1e7d8c369dcb3aef0
bvs <fabish@bk.ru>
2025-04-05 13:42:53 +0300

    Удалил easy_profiler, mongo, bson

--- googletest/main.cpp
+++ googletest/main.cpp
@@ -1,8 +1,5 @@
 #include "example.h"
 #include <gtest/gtest.h>
-#if defined(_MSC_VER)
-#include <easy/profiler.h>
-#endif
 
 static constexpr int kIn_0{100};
 static constexpr int kIn_1{10000};
@@ -15,23 +12,8 @@ struct ExampleTest : public testing::TestWithParam<test_value_t>
 {
 };
 
-struct Cat
-{
-    Cat(int i) {}
-    Cat &operator()(int i) {}
-};
-
 TEST_P(ExampleTest, case_dummy)
-{
-#if defined(_MSC_VER)
-    EASY_FUNCTION(profiler::colors::Cyan);
-#endif
-
-    auto schrodinger{
-        [](Cat)
-        { return Cat(1); }};
-        
-
+{  
     auto param{GetParam()};
     auto in{std::get<0>(param)};
     auto out{std::get<1>(param)};
@@ -43,9 +25,6 @@ TEST_P(ExampleTest, case_dummy)
 
 TEST_P(ExampleTest, case_dummy_noexept)
 {
-#if defined(_MSC_VER)
-    EASY_FUNCTION(profiler::colors::Red);
-#endif
     auto param{GetParam()};
     auto in{std::get<0>(param)};
     auto out{std::get<1>(param)};
@@ -59,15 +38,7 @@ INSTANTIATE_TEST_SUITE_P(ExampleTests, ExampleTest, testing::Values(std::make_tu
 
 int main(int argc, char **argv)
 {
-#if defined(_MSC_VER)
-    EASY_PROFILER_ENABLE;
-#endif
-
     ::testing::InitGoogleTest(&argc, argv);
     auto res{RUN_ALL_TESTS()};
-
-#if defined(_MSC_VER)
-    profiler::dumpBlocksToFile("test_profile.prof");
-#endif
     return res;
 }
